4194c164157ac9b3e9bffe6af0276c1d2d2d4089
AaronVelasco93 <aaronvelasco1933@outlook.com>
2022-09-21 10:50:35 -0500

    Mi primer programa  en C

---
+++ Clase2/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	//declaracion de variables
+	int numero1=1;
+	int numero2=2;
+	int nunero3=0;
+	
+	//operacion
+	
+	nunero3=numero1+numero2;
+	
+	
+	
+	return 0;
+}
---
+++ cadena.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+char nombre[]="carlos alberto de la cruz castelan";
+int edad=10;//una variable
+
+}
